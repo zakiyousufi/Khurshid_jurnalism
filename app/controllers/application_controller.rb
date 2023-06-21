4a93956b7c148657da7f23e2edd0b827cc30dfd4
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-06-22 00:53:59 +0430

    Update application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+  include JsonWebToken
+
+  # before_action :authenticate_request
+
+  private
+
+  def authenticate_request
+    header = request.headers['Authorization']
+    if header && header.split(' ').length == 2
+      token = header.split(' ').last
+      begin
+        decoded = jwt_decode(token)
+        @current_user = User.find(decoded[:user_id])
+      rescue JWT::DecodeError
+        render json: { error: 'Invalid token' }, status: :unauthorized
+      end
+    else
+      render json: { error: 'Missing or invalid Authorization header' }, status: :unauthorized
+    end
+  end
 end
