e9e109e6a7b23f23191bda58a1a72f6e5f7ceae5
Ahmad zaki Yousufi <ahmadzakiyousufi3@gmail.com>
2023-06-22 00:58:03 +0430

    Create routes for requists

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+  namespace :api do
+    namespace :v1 do
+      resources :teachers, only: [:index, :show, :create, :destroy]
+      resources :students, only: [:index, :show, :create, :destroy]
+      resources :events, only: [:index, :show, :create, :destroy]
+      resources :posts, only: [:index, :show, :create, :destroy]
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:create]
+  post '/auth/login', to: 'authentication#login'
 end
